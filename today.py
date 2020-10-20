45f7e8d085bca4b09b1ad6f193c2b81f9d4873a7
zhou u74a8 <1571224208@qq.com>
2020-10-20 17:31:44 +0800

    kh03 frame

---
+++ today.py
@@ -0,0 +1,44 @@
+import time
+import os
+
+tm = time.localtime()
+md1 = '''/*********************************
+文件名：'''
+md2 = '''
+题目：
+姓名：周璨
+学号：200512134
+时间：'''
+md3 = '''
+修改履历：
+*********************************/
+
+/*********************************
+NOTE: This file is transcoded into UTF-8.
+*********************************/
+
+/*********************************
+非编程类作业在此处作答
+*********************************/
+
+
+/******程序类作业在下面作答******/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+}
+'''
+
+folder = input("Work name: ")
+os.mkdir(folder)
+os.chdir(folder)
+num = int(input("Number: "))
+
+for i in range(1,num+1):
+	fname = folder+("00"+str(i))[-2:]+".cpp"
+	f = open(folder+("00"+str(i))[-2:]+".cpp","a",encoding="utf-8")
+	f.write(md1+fname+md2+str(tm.tm_year)+"."+("00"+str(tm.tm_mon))[-2:]+"."+("00"+str(tm.tm_mday))[-2:]+md3)
+	f.close()
