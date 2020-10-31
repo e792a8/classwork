471fc18653c4c524725d647f08ea3b1f0ad7e19d
zhou u74a8 <1571224208@qq.com>
2020-10-31 15:29:42 +0800

    sh03 ok

--- sh03/sh0303.cpp
+++ sh03/sh0303.cpp
@@ -1,21 +1,21 @@
 /*********************************
 文件名：sh0303.cpp
-题目：
+题目：输人一个华氏温度，要求输出摄氏温度。公式为c=0(F -32) ,输出要有文字说明,取两位小数。
 姓名：周璨
 学号：200512134
 时间：2020.10.30
 修改履历：
 *********************************/
 
-/*********************************
-非编程类作业在此处作答
-*********************************/
-
-/******程序类作业在下面作答******/
 #include <iostream>
+#include <cstdio>
 using namespace std;
 
 int main()
 {
-
+	double a;
+	cout << "Enter a degree in fahrenheit: ";
+	cin >> a;
+	printf("%.2lf degree fahrenheit is %.2lf degree centigrade.\n",a,(a-32)*5/9);
+	return 0;
 }
--- sh03/sh0316.cpp
+++ sh03/sh0316.cpp
@@ -1,21 +1,30 @@
 /*********************************
 文件名：sh0316.cpp
-题目：
+题目：输人一行字符,分别统计出其中英文字母、空格、数字和其他字符的个数。
 姓名：周璨
 学号：200512134
 时间：2020.10.30
 修改履历：
 *********************************/
 
-/*********************************
-非编程类作业在此处作答
-*********************************/
-
-/******程序类作业在下面作答******/
 #include <iostream>
 using namespace std;
 
 int main()
 {
-
+	char str[1024];
+	int c, s, d, o, i;
+	c=s=d=o=i=0;
+	cin.getline(str,1023);
+	for(;str[i];++i){
+		if(isdigit(str[i])) ++d;
+		else if(isalpha(str[i])) ++c;
+		else if(str[i]==' ') ++s;
+		else ++o;
+	}
+	cout << "Alphas: " << c << endl
+	<< "Spaces: " << s << endl
+	<< "Digits: " << d << endl
+	<< "Others: " << o << endl;
+	return 0;
 }
