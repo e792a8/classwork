32051e0030fd0e562d0e433ffd7760f3dbc6e310
zhou u74a8 <1571224208@qq.com>
2020-11-04 21:05:19 +0800

    sy03 ok

--- sy03/sy0305.cpp
+++ sy03/sy0305.cpp
@@ -1,21 +1,43 @@
 /*********************************
 文件名：sy0305.cpp
-题目：
+题目：5.	求一元二次方程的根
 姓名：周璨
 学号：200512134
 时间：2020.11.04
 修改履历：
 *********************************/
 
-/*********************************
-非编程类作业在此处作答
-*********************************/
-
-/******程序类作业在下面作答******/
-#include <iostream>
+#include <cstdio>
+#include <cmath>
 using namespace std;
 
 int main()
 {
-
+	double r, t, d,
+		a, b, c;
+	scanf("%lf%lf%lf",&a,&b,&c);
+	d = b*b-4*a*c;
+	t = sqrt(abs(d))/a/2;
+	r = -b/a/2;
+	if(abs(d)<1e-6)
+	{
+		printf("x1=x2=%.5lf",r);
+	}
+	else if(d>0)
+	{
+		printf(
+			"x1=%.5lf; x2=%.5lf\n",
+			r + t,
+			r - t
+		);
+	}
+	else
+	{
+		printf(
+			"x1=%.5lf%+.5lfi; "
+			"x2=%.5lf%+.5lfi",
+			r, t, r, -t
+		);
+	}
+	return 0;
 }
